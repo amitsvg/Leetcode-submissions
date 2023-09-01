bbd54bbeae62a140da1eababbe6a078cfeabb8da
Amit Kumar <72488615+amitsvg@users.noreply.github.com>
2023-09-01 23:03:41 +0530

    Time: 3 ms (89.82%), Space: 8.6 MB (14.67%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i=0; i<=n; i++){
+            bitset<32> b1(i);
+            ans.push_back(b1.count());
+        }
+        
+        return ans;
+        
+    }
+};

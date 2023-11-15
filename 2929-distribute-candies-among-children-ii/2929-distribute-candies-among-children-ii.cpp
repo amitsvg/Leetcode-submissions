ee3416ef442c5da3eca51b32494da73e1ad935fa
Amit Kumar <72488615+amitsvg@users.noreply.github.com>
2023-11-15 14:25:37 +0530

    Time: 34 ms (15.56%), Space: 6.3 MB (79.47%) - LeetHub

--- 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.cpp
+++ 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     
     long long distributeCandies(int n, int limit) {
+        
         long long ans=0;
         for(int i=0; i<=min(n, limit); i++){
             if(n-i <= 2*limit){
-                int upper = min(limit, n-i);
+                int upper = min(n-i, limit);
                 int lower = n-i-upper;
-                // if(upper>= lower)
-                    ans += upper - lower + 1;
+                ans+=upper-lower+1;
             }
         }
-        return ans;
-        
+            return ans;
     }
 };

2dbe629d988152f84434e3812c374490b20faf97
Amit Kumar <72488615+amitsvg@users.noreply.github.com>
2024-05-07 12:10:29 +0530

    Time: 2711 ms (5.02%), Space: 523.4 MB (5.01%) - LeetHub

--- 3138-minimum-length-of-anagram-concatenation/3138-minimum-length-of-anagram-concatenation.cpp
+++ 3138-minimum-length-of-anagram-concatenation/3138-minimum-length-of-anagram-concatenation.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
-    bool check(string p, string t){
-        int arr[26]={0};
-        // memset(arr,0,sizeof(arr));
-        for(auto i:p){
-            arr[i-'a']++;
+//     bool check(string p, string t){
+//         int arr[26]={0};
+//         // memset(arr,0,sizeof(arr));
+//         for(auto i:p){
+//             arr[i-'a']++;
+//         }
+//         for(auto i: t){
+//             arr[i-'a']--;
+//         }
+//         for(int i=0; i<26; i++){
+//             if(arr[i]!=0)return false;
+//         }
+//         return true;
+        
+//     }
+    bool ff(string s, int k){
+        int n =s.size();
+        unordered_map<int,int> mp1,mp2;
+        int i=0;
+        while(i<k){
+            mp1[s[i]]++;
+            i++;
         }
-        for(auto i: t){
-            arr[i-'a']--;
-        }
-        for(int i=0; i<26; i++){
-            if(arr[i]!=0)return false;
+        for(;i<n;i+=k){
+            for(int j=i; j<i+k; j++){
+                mp2[s[j]]++;
+            }
+            for(char ch='a'; ch<='z'; ch++){
+                if(mp1[ch] != mp2[ch]){
+                    return false;
+                }
+            }
+            mp2.clear();
         }
         return true;
         
@@ -22,22 +44,10 @@ public:
         for(int i=1;i*i<=n;i++){
             if(n%i==0)div.push_back(i),div.push_back(n/i);
         }
+        sort(begin(div),end(div));
         for(auto k: div){
-            string t=s.substr(0,k);
-            bool found = false;
-            for(int i=k; i<n; i+=k){
-                string p = s.substr(i,k);
-                if(check(p,t)){
-                    found=true;
-                }
-                else{
-                    found=false;
-                    break;
-                }
-            }
-            if(found){
-                ans = min(ans, k);
-            }
+            bool a = ff(s,k);
+            if(a) return k;
         }
         return ans;
         

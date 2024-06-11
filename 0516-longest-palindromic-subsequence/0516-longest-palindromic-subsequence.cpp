0e66a3fbf155c87f969f407a19892c4fad1790f7
Amit Kumar <72488615+amitsvg@users.noreply.github.com>
2024-06-11 07:23:30 +0530

    Time: 59 ms (63.29%), Space: 12.1 MB (77.44%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int lcs(string x, string y){
-        int m=x.size(),n=y.size();
+        int m=x.size(), n=y.size();
         int t[m+1][n+1];
-        
         for(int i=0; i<m+1; i++){
             for(int j=0; j<n+1; j++){
-                t[i][j]=0;
-            }
-        }
-        
-        for(int i=1; i<m+1; i++){
-            for(int j=1; j<n+1; j++){
-                if(x[i-1] == y[j-1]){
-                    t[i][j] = 1 + t[i-1][j-1];
+                if(i==0 || j==0) t[i][j]=0;
+                else if(x[i-1] == y[j-1]){
+                    t[i][j]= 1+t[i-1][j-1];
                 }
                 else{
-                    t[i][j] = max(t[i-1][j], t[i][j-1]);
+                    t[i][j]=max(t[i-1][j], t[i][j-1]);
                 }
             }
         }
@@ -26,7 +20,6 @@ public:
         string t=s;
         reverse(t.begin(), t.end());
         return lcs(s,t);
-        
         
     }
 };
